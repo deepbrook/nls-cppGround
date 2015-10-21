3b6e4aa1ffc98ba5633c3ae4eda7d9c0b14dc03d
nlsdfnbch <23okrs35+github@mykolab.com>
2015-10-21 10:50:21 +0200

    added O(1) queue functions, and doublylinked lists data structures;

--- structurist/doublylinkedlists.cpp
+++ structurist/doublylinkedlists.cpp
@@ -1,1 +1,62 @@
 
+
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+
+struct ListItem {
+    unsigned int data;
+    struct ListItem *next;
+    struct ListItem *previous;
+};
+
+struct List{
+    struct ListItem *head;
+    struct ListItem *tail;
+
+};
+
+void push(int value, ListItem **head)
+{
+    ListItem *newEntry; // register a name with your machine for a var
+    newEntry = (ListItem*)malloc(sizeof(struct ListItem)); // reserve memory for an object with the size of a listItem
+    newEntry->data = value;
+    newEntry->next = *head;
+    newEntry->previous = NULL;
+    *head->previous = newEntry;
+    head = newEntry;
+    return;
+}
+
+
+unsigned int pop(ListItem **tail)
+{
+    ListItem* newTail = *tail->previous;
+    unsigned int val = *tail->data;
+    *tail->previous->next = NULL;
+    free(*tail);
+    tail = newTail;
+    return val;
+}
+
+int main()
+{
+
+    ListItem *head = NULL; //empty linked list, *next is null
+
+
+    push_tail(4,&head);
+    push_tail(17,&head);
+    push_tail(19,&head);
+    pop_tail(&head);
+    push_tail(27,&head);
+
+    for (ListItem* i = head; i!=0; i = i->next){
+        cout << i->data;
+    }
+
+
+}
+
+
